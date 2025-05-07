861df300c5c0d50b2841dbd42ffe8e65cffde348
jefta-jose <ndegwajeff4@gmail.com>
2025-05-07 18:31:29 +0300

    s3 bucket access policy

--- linkex_upload_terraform/main.tf
+++ linkex_upload_terraform/main.tf
@@ -41,10 +41,41 @@ resource "aws_s3_bucket" "upload_bucket" {
   }
 }
 
+# myself as user was created in aws ui by Aaron
+# then myself, I was able to create an access key and secret key 
+# which I later used in my ci-cd to provision resources
+
+# this is the same -> we are creating a user 
+# and this user will have secrets and access key that they can use
+# depending on their level of permission
+
+
+# in this case we have just created a user who so far has no access to any resources
 resource "aws_iam_user" "linkex_upload_user" {
   name = "linkex-upload-user"
 
   tags = {
     Name = "linkex-upload-user"
+  }
+}
+
+
+# we need to grant our user read && write to our s3
+data "aws_iam_policy_document" "s3_bucket_access" {
+  statement {
+    effect = "Allow"
+
+    # this block defines what actions can be done on the  bucket
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+
+    # this block defines what can you do the above to
+    resources = [
+      aws_s3_bucket.upload_bucket.arn,
+    ]
+
   }
 }
